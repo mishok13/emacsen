f5c293223bc1be2e72259c49bd1f08461fef49b9
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-03-01 09:22:11 +0100

    Fixed scrolling

--- goodies/scrolling.el
+++ goodies/scrolling.el
@@ -1,5 +1,6 @@
 ;; Setup scrolling behaviour
 
 ;; make scroll behave more like notepad, he-he
-(setq scroll-conservatively 50)
+(setq scroll-conservatively 10000)
+(setq scroll-step 1)
 (setq scroll-preserve-screen-position 't)
