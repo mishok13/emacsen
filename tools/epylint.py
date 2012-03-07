9e56bad9db4fd7e5d2120ab7b4e504b21673adad
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-03-07 11:30:13 +0100

    Fixed pylint warnings in epylint (irony?)

--- tools/epylint.py
+++ tools/epylint.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python
 
+"""Pylint runner for Emacs"""
+
+from __future__ import print_function
+
 import re
 import argparse
-from os import environ
 
 from subprocess import Popen, PIPE
 
 def main(source, working_dir):
+    """Main entry point"""
     if working_dir:
-        command = 'PYTHONPATH={} pylint -f parseable -r n {}'.format(working_dir.strip(), source)
+        command = 'PYTHONPATH={} pylint -f parseable -r n {}'.format(
+            working_dir.strip(), source)
     else:
         command = "pylint -f parseable -r n {}".format(source)
-    with open('/tmp/pylint.out', 'a') as f:
-        f.write('command is: {}\n'.format(command))
-    p = Popen(command, shell=True, stdout=PIPE)
-    stdout = p.communicate()[0]
-    fullpattern = re.compile("(.*?):" # filename
-                             "([0-9]):" # line number
-                             "\[([EFIRWC])" # error type
-                             "([0-9]+).*?\]" # the error number and context (unused)
-                             "(.*)" # description
-                             )
+    process = Popen(command, shell=True, stdout=PIPE)
+    stdout = process.communicate()[0]
     pattern = re.compile("\[([EFIRWC])" # error type
                          "([0-9]+).*?\]" # the error number and context (unused)
                          "(.*)" # description
         )
 
     for line in stdout.splitlines():
-        line = line.strip()
         try:
-            filename, linenum, line = line.split(':', 2)
+            filename, linenum, line = line.strip().split(':', 2)
         except Exception:
             pass
         match = pattern.match(line.strip())
         if match:
             errtype, errnum, description = match.groups()
             description = description.strip()
-            if errtype in 'EF':
-                msg = 'Error'
-            else:
-                msg = 'Warning'
-            print "%s %s%s %s at %s line %s." % (msg, errtype, errnum,
-                                                 description, filename, linenum)
+            level = {'E': 'Error', 'F': 'Error'}.get(errtype, 'Warning')
+            print(('{level} {error_type}{error_id} '
+                   '{description} {path} {line}').format(
+                       level=level, error_type=errtype,
+                       error_id=errnum, description=description,
+                       path=filename, line=linenum))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Emacs Pylint integration')
