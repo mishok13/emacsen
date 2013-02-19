9cff17e95842c2d8ef5648086bb4f3f14934b40c
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-02-19 11:17:44 +0100

    More updates

--- goodies/flymake-init.el
+++ goodies/flymake-init.el
@@ -4,4 +4,6 @@
 (delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
 (delete '("\\.hs?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
 (delete '("\\.tex\\'" flymake-simple-tex-init) flymake-allowed-file-name-masks)
+(delete '("\\.xml\\'" flymake-xml-init) flymake-allowed-file-name-masks)
+(delete '("\\.php[345]?\\'" flymake-php-init) flymake-allowed-file-name-masks)
 (delete '("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) flymake-allowed-file-name-masks)
---
+++ languages/sgml.el
@@ -0,0 +1,4 @@
+(require 'zencoding-mode)
+
+(add-hook 'sgml-mode-hook 'zencoding-mode)
+(setq-default zencoding-preview-default nil)
