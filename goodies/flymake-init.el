54c1add3bbb2b1962734d010fce68cf5fdf1c1f8
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-04-18 13:59:25 +0200

    Switched to flycheck

--- goodies/flymake-init.el
+++ goodies/flymake-init.el
@@ -1,9 +1,2 @@
-(require 'flymake)
-
-;; remove offending checkers for flymake
-(delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
-(delete '("\\.hs?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
-(delete '("\\.tex\\'" flymake-simple-tex-init) flymake-allowed-file-name-masks)
-(delete '("\\.xml\\'" flymake-xml-init) flymake-allowed-file-name-masks)
-(delete '("\\.php[345]?\\'" flymake-php-init) flymake-allowed-file-name-masks)
-(delete '("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) flymake-allowed-file-name-masks)
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
