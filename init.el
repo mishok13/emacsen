238e468f51493ed963771b868e129d2623126e38
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-04-18 13:58:47 +0200

    Now packages are automatically checked on the startup

--- init.el
+++ init.el
@@ -17,32 +17,30 @@
   (package-refresh-contents))
 
 ;; This should be moved to another module, to speedup startup time
-;; (defvar prelude-packages
-;;   '(ack-and-a-half auctex clojure-mode expand-region
-;;                    autopair fill-column-indicator flymake-cursor
-;;                    helm highlight-parentheses
-;;                    magit markdown-mode paredit projectile
-;;                    nrepl nrepl-ritz org php-mode powerline
-;;                    rainbow-mode python-mode python-pep8 smex
-;;                    twittering-mode virtualenv yasnippet zencoding-mode)
-;;   "A list of packages to ensure are installed at launch.")
+(defvar prelude-packages
+  '(clojure-snippets ack-and-a-half auctex autopair expand-region
+    fill-column-indicator flycheck flymake-cursor helm highlight-parentheses
+    kibit-mode magit markdown-mode nrepl-ritz fringe-helper nrepl clojure-mode
+    org paredit php-mode powerline projectile dash python-mode python-pep8
+    rainbow-mode s smex twittering-mode virtualenv yasnippet zencoding-mode)
+  "A list of packages to ensure are installed at launch.")
 
-;; (defun prelude-packages-installed-p ()
-;;   (loop for p in prelude-packages
-;;         when (not (package-installed-p p)) do (return nil)
-;;         finally (return t)))
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
 
-;; (unless (prelude-packages-installed-p)
-;;   ;; check for new packages (package versions)
-;;   (message "%s" "Emacs Prelude is now refreshing its package database...")
-;;   (package-refresh-contents)
-;;   (message "%s" " done.")
-;;   ;; install the missing packages
-;;   (dolist (p prelude-packages)
-;;     (when (not (package-installed-p p))
-;;       (package-install p))))
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
-;; (provide 'prelude-packages)
+(provide 'prelude-packages)
 
 
 
