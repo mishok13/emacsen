32876a2d50017a670c3fb1c5c4021fab2e744dd4
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-06-05 15:41:21 +0200

    The hell was I thinking?

--- goodies/keys.el
+++ goodies/keys.el
@@ -27,8 +27,3 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-(global-set-key (kbd "M-j")
-            (lambda ()
-              (interactive)
-              (join-line -1)))
