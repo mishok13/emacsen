6829026b95d22d1d2643a2b56618c86c6ab71d25
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-06-05 15:42:37 +0200

    Some prettyfying of Clojure code

--- languages/clojure.el
+++ languages/clojure.el
@@ -5,6 +5,34 @@
 (defun my-nrepl-mode-setup ()
   (require 'nrepl-ritz))
 
+
+(defun mishok-pretty-partial ()
+  (font-lock-add-keywords nil
+                          `(("(\\(partial\\)[[:space:]]"
+                             (0 (progn (compose-region (match-beginning 1)
+                                                       (match-end 1)
+                                                       "\u03a0"
+                                                       'decompose-region)))))))
+
+(defun mishok-pretty-comp ()
+  (font-lock-add-keywords nil
+                          `(("(\\(comp\\)[[:space:]]"
+                             (0 (progn (compose-region (match-beginning 1)
+                                                       (match-end 1)
+                                                       "\u2218"
+                                                       'decompose-region)))))))
+
+(defun mishok-pretty-fn ()
+  (font-lock-add-keywords nil
+                          `(("(\\(\\<fn\\>\\)"
+                             (0 (progn (compose-region (match-beginning 1)
+                                                       (match-end 1)
+                                                       "\u0192"
+                                                       'decompose-region)))))))
+
+(add-hook 'clojure-mode-hook 'mishok-pretty-fn)
+(add-hook 'clojure-mode-hook 'mishok-pretty-partial)
+(add-hook 'clojure-mode-hook 'mishok-pretty-comp)
 (add-hook 'clojure-mode-hook 'fci-mode)
 (add-hook 'clojure-mode-hook 'flycheck-mode)
 (add-hook 'clojure-mode-hook 'kibit-mode)
