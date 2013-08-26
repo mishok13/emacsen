9386c9dbaf7154cca923e1157c19983dec542b3a
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-08-26 09:22:06 +0200

    Removing some unused stuff

--- goodies/nrepl.el
+++
@@ -1,6 +0,0 @@
-(require 'nrepl)
-(require 'paredit)
-
-(add-hook 'nrepl-connected-hook 'bury-buffer) ;;; don't send me to the repl on connect
-(add-hook 'nrepl-connected-hook 'reset-nrepl-connection-to-default) ;;; always default to first connection
-(add-hook 'nrepl-mode-hook 'paredit-mode)
