060ef79a535363fa30b936673996f2bcb1bddfd8
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-12-12 12:47:25 +0100

    Properly init language configs

--- lib/lang/mishok-c.el
+++ lib/lang/mishok-c.el
@@ -6,3 +6,5 @@
             (c-set-style "k&r")
             (setq c-basic-offset 4)
 	    (setq indent-tabs-mode nil)))
+
+(provide 'mishok-c)
--- lib/lang/mishok-sgml.el
+++ lib/lang/mishok-sgml.el
@@ -2,3 +2,5 @@
 (setq emmet-preview-default nil)
 (define-key emmet-mode-keymap (kbd "C-j") 'newline-and-indent)
 (define-key emmet-mode-keymap (kbd "C-m") 'emmet-expand-line)
+
+(provide 'mishok-sgml)
