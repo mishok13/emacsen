48f11d341b5b8689a126ef2826ac17071dedc0b6
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2013-12-15 16:16:04 +0100

    Full rewrite of editing tweaks

--- lib/mishok-editing.el
+++ lib/mishok-editing.el
@@ -2,25 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Set up backups
+(require 'desktop)
+(require 'saveplace)
+(require 'ido)
+(require 'flx-ido)
+(require 'recentf)
+(require 'flyspell)
+
 (ido-mode t)
+(ido-everywhere t)
+(flx-ido-mode t)
+
 (setq-default indent-tabs-mode nil)
 
+;; Set up backups
+(setq backup-by-copying t
+      backup-directory-alist '(("." . "~/.emacs.d/.backups"))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
-(setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.emacs.d/backups"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+(visual-line-mode t)
 
 
 ;; clear up files before saving them
 
 (defun delete-trailing-blank-lines ()
-  "Deletes all blank lines at the end of the file and leaves single newline character."
+  "Delete all blank lines at the end of the file and leave single newline character."
   (interactive)
   (save-excursion
     (goto-char (point-max))
@@ -36,10 +45,7 @@
 (setq x-select-enable-clipboard t)
 
 ;; Save history
-
-(require 'desktop)
-
-(desktop-save-mode 1)
+(desktop-save-mode t)
 (setq history-length 250)
 
 (add-to-list 'desktop-globals-to-save 'file-name-history)
@@ -51,12 +57,9 @@
 
 
 ;; Save point position between sessions
-(require 'saveplace)
 (setq-default save-place t)
 (setq save-place-file (expand-file-name ".places" user-emacs-directory))
 
-(require 'ido)
-(require 'recentf)
 
 (setq recentf-exclude
       (append recentf-exclude
@@ -68,7 +71,7 @@
 
 ;; Get ido to handle recentf results
 (defun ido-recentf-open ()
-  "Use `ido-completing-read' to \\[find-file] a recent file"
+  "Use `ido-completing-read' to \\[find-file] a recent file."
   (interactive)
   (if (find-file (ido-completing-read "Find recent file: " recentf-list))
       (message "Opening file...")
@@ -76,17 +79,18 @@
 
 (global-set-key (kbd "C-x C-r") 'ido-recentf-open)
 
-(require 'flyspell)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 (define-key flyspell-mode-map (kbd "<f2>") 'ispell-word)
 (define-key flyspell-mode-map (kbd "<f3>") 'flyspell-buffer)
 (define-key flyspell-mode-map (kbd "<f4>") 'flyspell-check-previous-highlighted-word)
 (defun flyspell-check-next-highlighted-word ()
-  "Custom function to spell check next highlighted word"
+  "Custom function to spell check next highlighted word."
   (interactive)
   (flyspell-goto-next-error)
   (ispell-word))
 (define-key flyspell-mode-map (kbd "<f5>") 'flyspell-check-next-highlighted-word)
+
+(projectile-global-mode)
 
 (provide 'mishok-editing)
 ;;; mishok-editing ends here
