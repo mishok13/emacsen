947bb2194857baaf4cb90d2d492ce8d41b44901f
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-11-20 21:26:46 +0100

    Automatically show trailing whitespace when coding

--- lib/lang/mishok-prog.el
+++ lib/lang/mishok-prog.el
@@ -7,7 +7,10 @@
 (require 'prog-mode)
 (require 'fill-column-indicator)
 (require 'flycheck)
+(require 'aggressive-indent)
+(require 'flyspell)
 
+(add-hook 'prog-mode-hook #'aggressive-indent-mode)
 (setq-default fci-rule-column 80)
 (add-hook 'prog-mode-hook 'fci-mode)
 (which-function-mode t)
@@ -16,8 +19,7 @@
 (defun flymake-xml-init ())
 (defun flymake-java-init ())
 
-(require 'flyspell)
-
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 (provide 'mishok-prog)
