d22eadc242593adf0648cbc40c936809373a35f5
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-11-25 15:39:50 +0100

    Add JSX support

---
+++ lib/lang/mishok-jsx.el
@@ -0,0 +1,9 @@
+;;; mishok-jsx --- JSX support
+;;; Commentary:
+;;; Code:
+
+(require 'jsx-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+(setq jsx-indent-level 2)
+
+;;; mishok-jsx ends here
