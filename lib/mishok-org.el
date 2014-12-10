61c31893f18519e06b848a6f8574d2516a69a3fa
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-12-10 11:30:30 +0100

    Trying to return back to good old Org days.

--- lib/mishok-org.el
+++ lib/mishok-org.el
@@ -4,15 +4,25 @@
 
 (require 'org)
 
-(setq org-directory "~/Dropbox/org/")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-directory (expand-file-name "~/Dropbox/org/"))
+(defun org-path (tail) (concat org-directory tail))
+(setq org-default-notes-file (org-path "notes.org"))
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline (concat org-directory "/todo.org") "Tasks")
-             "* TODO %?\n  %i\n  %a")
-        ("d" "Dutch" entry (file (concat org-directory "/dutch.org"))
-             "* On %U:\n%?  %i\n")
-        ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
-             "* %?\nEntered on %U\n  %i\n  %a")))
+      '(("t" "Todo" entry (file+headline (org-path "todo.org") "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("d" "Dutch" entry (file (org-path "dutch.org"))
+         "* On %U:\n%?  %i\n")
+        ("j" "Journal" entry (file+datetree (org-path "journal.org"))
+         "* %?\nEntered on %U\n  %i\n  %a")))
+
+
+
+(setq org-agenda-files (mapcar 'org-path '("work.org")))
+
+(setq org-clock-persist 'history)
+(setq org-log-done 'note)
+(org-clock-persistence-insinuate)
+
 
 (provide 'mishok-org)
 ;;; mishok-org ends here
