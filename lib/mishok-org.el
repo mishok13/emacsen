4c2f6e173d4530a16fa7ab4606b97d5c14a9109f
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-07 18:46:41 +0200

    Let org-clock automatically identify idle time

--- lib/mishok-org.el
+++ lib/mishok-org.el
@@ -15,14 +15,16 @@
         ("j" "Journal" entry (file+datetree (org-path "journal.org"))
          "* %?\nEntered on %U\n  %i\n  %a")))
 
-
-
-(setq org-agenda-files (mapcar 'org-path '("work.org")))
+(setq org-agenda-files (mapcar 'org-path '("work.org" "nonwork.org")))
+;; Set idle time to 10 minutes (10 minutes of idling will lead to
+;; org-clock asking whether clock-out has to be performed)
+(setq org-clock-idle-time 10)
 
 (setq org-clock-persist 'history)
 (setq org-log-done 'note)
 (org-clock-persistence-insinuate)
 
+(add-to-list 'org-modules 'org-habit t)
 
 (provide 'mishok-org)
 ;;; mishok-org ends here
