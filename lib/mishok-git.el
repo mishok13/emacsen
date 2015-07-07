f21308d301f91dae7bca1a2fe81753c67de441fe
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-07 22:51:28 +0200

    Refactored Git setup to make use of use-package

--- lib/mishok-git.el
+++ lib/mishok-git.el
@@ -1,22 +1,21 @@
-;; full screen magit-status
+;;; mishok-git --- Magit setup
+;;; Commentary:
+;;; Code:
 
-(require 'magit)
-(require 'magit-svn)
-
-(add-hook 'magit-mode-hook 'magit-load-config-extensions)
-(add-hook 'magit-commit-mode-hook 'flyspell-mode)
-
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(use-package magit
+  :bind (("<f7>" . magit-status))
+  :init
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+  (add-hook 'git-commit-mode-hook 'flyspell-mode)
+  :config
+  (defadvice magit-quit-window (around magit-restore-screen activate)
+    (let ((current-mode major-mode))
+      ad-do-it
+      (when (eq 'magit-status-mode current-mode)
+        (jump-to-register :magit-fullscreen)))))
 
 (provide 'mishok-git)
+;;; mishok-git.el ends here
