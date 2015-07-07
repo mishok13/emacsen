46eee79af52e3768ac66b422d9777c53b0fc9d90
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-07 22:59:51 +0200

    Refactored twittering-mode setup to use use-package

--- lib/mishok-twit.el
+++ lib/mishok-twit.el
@@ -1,4 +1,15 @@
-(require 'twittering-mode)
-(setq twittering-use-master-password t)
+;;; mishok-twit -- Twittering-mode setup
+;;; Commentary:
+;;; Code:
+(require 'use-package)
+
+(use-package twittering-mode
+  :ensure t
+  :init
+  ;; Use password for accessing private key
+  (setq twittering-use-master-password t)
+  ;; Don't put message in echo area
+  (setq twittering-url-show-status nil))
 
 (provide 'mishok-twit)
+;;; mishok-twit.el ends here
