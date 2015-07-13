91da652c907b43e259109111339ea5778b848120
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-13 12:07:00 +0200

    Removed JSX

--- lib/lang/mishok-jsx.el
+++
@@ -1,9 +0,0 @@
-;;; mishok-jsx --- JSX support
-;;; Commentary:
-;;; Code:
-
-(require 'jsx-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-(setq jsx-indent-level 2)
-
-;;; mishok-jsx ends here
