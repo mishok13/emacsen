88292f1d6c5b07cede605090131901ecfe994652
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-13 12:41:22 +0200

    Minor fixes (still needs to be use-packagized)

--- osx.el
+++ osx.el
@@ -1,5 +1,6 @@
-;; (require 'maxframe)
-
+;;; osx -- OS X related setup
+;;; Commentary:
+;;; Code:
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (replace-regexp-in-string
                           "[ \t\n]*$"
@@ -8,15 +9,10 @@
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
-;; (defun set-exec-path-from-shell-PATH ()
-;;   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-;;     (setenv "PATH" path-from-shell)
-;;     (setq exec-path (split-string path-from-shell path-separator))))
-
 ;; Need to make sure Emacs successfully inherits PATH variable
 (set-exec-path-from-shell-PATH)
 
 (setq mac-allow-anti-aliasing t)
 
-;; Turn to almost-full-screen
-;; (maximize-frame)
+(provide 'osx)
+;;; osx.el ends here
