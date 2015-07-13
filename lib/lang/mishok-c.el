c687834d9713e5a1b669da1221fc19173d215683
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-13 12:04:48 +0200

    C&CC-mode use-packaged

--- lib/lang/mishok-c.el
+++ lib/lang/mishok-c.el
@@ -1,10 +1,15 @@
-(require 'cc-mode)
+;;; mishok-c -- C/C++ setup
+;;; Commentary:
+;;; Code:
+(require 'use-package)
 
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (c-set-style "k&r")
-            (setq c-basic-offset 4)
-	    (setq indent-tabs-mode nil)))
+(use-package cc-mode
+  :ensure t
+  :config
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (c-set-style "k&r")
+              (setq indent-tabs-mode nil))))
 
 (provide 'mishok-c)
+;;; mishok-c.el ends here
