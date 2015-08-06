5247f07e5d93ad7ce72449bb7828fa6e2f37cff4
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-08-06 11:50:17 +0200

    Enable fuzzy matching for helm

--- lib/mishok-commands.el
+++ lib/mishok-commands.el
@@ -6,6 +6,11 @@
 (use-package helm
   :init
   (setq helm-M-x-fuzzy-match t)
+  ;; Enable fuzzy matching for recent files
+  (setq helm-recentf-fuzzy-match t)
+  ;; Prevent helm from annoying message pop up when fuzzy-completing
+  ;; https://github.com/emacs-helm/helm/issues/550
+  (setq helm-exit-idle-delay 0)
   :bind (("M-x" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-x b" . helm-mini)))
