9bd69b43beac5061a3057b98a03b786ba8e71882
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-12-02 12:33:41 +0100

    Some small messages

--- lib/lang/mishok-py.el
+++ lib/lang/mishok-py.el
@@ -9,8 +9,26 @@
   (add-hook 'python-mode-hook 'smartparens-mode)
   (add-hook 'python-mode-hook 'electric-indent-mode))
 
+(defun mishok/python-flycheck-setup ()
+  (let ((exec-path (python-shell-calculate-exec-path)))
+    (setq-local flycheck-python-pylint-executable (executable-find "pylint"))
+    (setq-local flycheck-python-flake8-executable (executable-find "flake8"))))
+
+(defun mishok/workon (&optional VENV-NAME)
+  (interactive)
+  (venv-workon VENV-NAME)
+  (mishok/python-flycheck-setup)
+  (flycheck-mode nil)
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
+  (flycheck-mode t))
+
 (use-package python
-  :bind ("C-j" . newline-and-indent))
+  :bind ("C-j" . newline-and-indent)
+  :init
+  (add-hook 'hack-local-variables-hook 'mishok/python-flycheck-setup nil 'local))
+
+(use-package jedi
+  :ensure t)
 
 (use-package virtualenvwrapper
   :ensure t
