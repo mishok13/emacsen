3cc9a50fe9b530b860366ea3f65119b15da534ff
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2017-10-16 18:06:49 +0100

    Implements racer and company support for Rust

--- lib/lang/mishok-rust.el
+++ lib/lang/mishok-rust.el
@@ -9,5 +9,18 @@
   :config
   (add-hook 'rust-mode-hook 'smartparens-mode))
 
+(use-package racer
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'racer-mode)
+  (add-hook 'racer-mode-hook 'eldoc-mode))
+
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'racer-mode-hook #'company-mode)
+  ;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t))
+
 (provide 'mishok-rust)
 ;;; mishok-rust ends here
