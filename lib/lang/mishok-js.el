bb44b3184a82c5cb2a94ea824cd8ad0562943f63
Andrii Mishkovskyi <mishok13@gmail.com>
2021-04-02 18:45:40 +0200

    More minor fixes

--- lib/lang/mishok-js.el
+++ lib/lang/mishok-js.el
@@ -20,12 +20,23 @@
   :config
   (setq json-reformat:indent-width 2))
 
+(use-package web-mode
+  :straight t)
+
 (use-package tide
   :straight t
   :config
+  (require 'web-mode)
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'tide-setup)
-  (add-hook 'typescript-mode-hook 'smartparens-mode))
+  (add-hook 'typescript-mode-hook 'smartparens-mode)
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
 
 (provide 'mishok-js)
 ;;; mishok-js ends here
