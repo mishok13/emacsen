2e8d77004de3b09d095a024ac32174277c1efaf2
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2023-09-20 20:51:33 +0200

    Cleaning up org setup (#15)
    Co-authored-by: Andrii Mishkovskyi <mishok13@users.noreply.github.com>

--- init.el
+++ init.el
@@ -520,23 +520,16 @@
          ("<down>" . windmove-down)))
 
 (use-package org
-  :straight t
+  :ensure t
   :bind (("<f8>" . org-capture)
          ("<f10>" . org-agenda))
+  :custom
+  (org-default-notes-file (org-path "notes.org"))
+  (org-directory (expand-file-name "~/nonwork/kitchensink/notes/org/"))
   :init
-  (defun org-path (tail) (concat org-directory tail))
   (setq org-log-done 'note)
   (org-clock-persistence-insinuate)
   (add-to-list 'org-modules 'org-habit t)
-  (setq org-directory (expand-file-name "~/Dropbox/org/"))
-  (setq org-default-notes-file (org-path "notes.org"))
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline (org-path "todo.org") "Tasks")
-           "* TODO %?\n  %i\n  %a")
-          ("d" "Dutch" entry (file (org-path "dutch.org"))
-           "* On %U:\n%?  %i\n")
-          ("j" "Journal" entry (file+datetree (org-path "journal.org"))
-           "* %?\nEntered on %U\n  %i\n  %a")))
 
   (setq org-agenda-files (mapcar 'org-path '("work.org" "nonwork.org")))
   :config
